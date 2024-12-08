78451cece0c1d98a178b4f96f33029e5f68ac353
JPMechanics12 <161663033+JPMechanics12@users.noreply.github.com>
2024-12-08 12:34:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['register'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     
-    <title>Login | Modern Design</title>
+    <title>MAFLIX</title>
     <style>
         /* Animation Container */
         .animation-container {
